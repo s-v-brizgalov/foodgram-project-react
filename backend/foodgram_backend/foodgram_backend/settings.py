e838779095f593fb0a6351bfcd2e4bfbc03c2a14
Сергей Бризгалов <s.v.brizgalov@yandex.ru>
2023-12-22 23:26:01 +0300

    passwordishe

--- backend/foodgram_backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/foodgram_backend/settings.py
@@ -100,7 +100,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'django'),
         'USER': os.getenv('POSTGRES_USER', 'django'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'django_password'),
+        # 'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'django_password'),
+        'PASSWORD': 'django_password',
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432)
     }
