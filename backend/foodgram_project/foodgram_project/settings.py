80e8648d9470df0be9f905b71778be4161205e83
Сергей Бризгалов <s.v.brizgalov@yandex.ru>
2023-12-25 15:47:48 +0300

    try

--- backend/foodgram_project/foodgram_project/settings.py
+++ backend/foodgram_project/foodgram_project/settings.py
@@ -8,7 +8,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 # DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 DEBUG = True
-# ALLOWED_HOSTS = ['158.160.18.225', '127.0.0.1', 'localhost', 'brizgalov.ru']
+# ALLOWED_HOSTS = ['158.160.24.32', '127.0.0.1', 'localhost', 'brizgalov.ru']
 ALLOWED_HOSTS = ['*']
 
 
