0f90f2d86ee0f3496bbc5cfff9ddd495a439f23e
Сергей Бризгалов <s.v.brizgalov@yandex.ru>
2024-01-16 03:59:42 +0300

    пока лучше некуда

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,7 +2,12 @@ from django.http import FileResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser import views
-from recipes.models import FavoriteRecipe, Follow, Ingredient, Recipe, RecipeIngredient, ShoppingCart, Tag
+from recipes.models import (
+    FavoriteRecipe, Follow,
+    Ingredient, Recipe,
+    RecipeIngredient,
+    ShoppingCart, Tag
+)
 from rest_framework import status, viewsets
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
