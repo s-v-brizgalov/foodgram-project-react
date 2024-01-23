449bcbb1e0f666c3e06820f48aa348b084c019bf
Сергей Бризгалов <s.v.brizgalov@yandex.ru>
2024-01-24 01:18:11 +0300

    local nginx, settings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-MEDIA_URL = 'https://brizgalov.ru/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CSV_FILES_DIR = os.path.join(BASE_DIR, 'data')
